3da562790fdf62e294fcdbe2731f04308d1c5f9f
Justin McLaren <justinmclaren63@gmail.com>
2025-09-21 23:35:09 -0700

    Replace AppIcon PNGs with a single vector asset

--- Project.swift
+++ Project.swift
@@ -7,6 +7,10 @@ let organizationName = "AI Shopper"
 let project = Project(
     name: appName,
     organizationName: organizationName,
+    packages: [
+        .remote(url: "https://github.com/groue/GRDB.swift", requirement: .upToNextMajor(from: "6.0.0")),
+        .remote(url: "https://github.com/kean/Pulse", requirement: .upToNextMajor(from: "4.0.0"))
+    ],
     targets: [
         .target(
             name: appName,
@@ -26,25 +30,65 @@ let project = Project(
                 .pre(script: "swift Scripts/embed_user_scripts.swift", name: "EmbedUserScripts")
             ],
             dependencies: [
-                .external(name: "GRDB"),
-                .external(name: "Pulse")
+                .package(product: "GRDB"),
+                .package(product: "Pulse")
             ]
         ),
-        .unitTests(
+        .target(
             name: "AppTests",
+            destinations: .iOS,
+            product: .unitTests,
             bundleId: "\(bundleId).tests",
             sources: ["App/Tests/AppTests/**"],
             dependencies: [.target(name: appName)]
         ),
-        .uiTests(
+        .target(
             name: "UITests",
+            destinations: .iOS,
+            product: .uiTests,
             bundleId: "\(bundleId).uitests",
             sources: ["App/Tests/UITests/**"],
             dependencies: [.target(name: appName)]
         )
     ],
-    packages: [
-        .package(url: "https://github.com/groue/GRDB.swift", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/kean/Pulse", .upToNextMajor(from: "4.0.0"))
+    schemes: [
+        Scheme.scheme(
+            name: "\(appName)-Debug",
+            buildAction: .buildAction(targets: [
+                .target(appName),
+                "AppTests",
+                "UITests"
+            ]),
+            testAction: .targets([
+                "AppTests",
+                "UITests"
+            ]),
+            runAction: .runAction(
+                configuration: .debug,
+                executable: .target(appName)
+            ),
+            archiveAction: .archiveAction(configuration: .release),
+            profileAction: .profileAction(
+                configuration: .release,
+                executable: .target(appName)
+            ),
+            analyzeAction: .analyzeAction(configuration: .debug)
+        ),
+        Scheme.scheme(
+            name: "\(appName)-Release",
+            buildAction: .buildAction(targets: [
+                .target(appName)
+            ]),
+            runAction: .runAction(
+                configuration: .release,
+                executable: .target(appName)
+            ),
+            archiveAction: .archiveAction(configuration: .release),
+            profileAction: .profileAction(
+                configuration: .release,
+                executable: .target(appName)
+            ),
+            analyzeAction: .analyzeAction(configuration: .release)
+        )
     ]
 )
