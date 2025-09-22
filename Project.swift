78be081203d6a6ba66bf07cb161a8e71a02ac009
Justin McLaren <justinmclaren63@gmail.com>
2025-09-21 19:29:25 -0700

    Fix Tuist package ordering in manifest

--- Project.swift
+++ Project.swift
@@ -7,6 +7,10 @@ let organizationName = "AI Shopper"
 let project = Project(
     name: appName,
     organizationName: organizationName,
+    packages: [
+        .remote(url: "https://github.com/groue/GRDB.swift", requirement: .upToNextMajor(from: "6.0.0")),
+        .remote(url: "https://github.com/kean/Pulse", requirement: .upToNextMajor(from: "4.0.0"))
+    ],
     targets: [
         .target(
             name: appName,
@@ -46,9 +50,5 @@ let project = Project(
             sources: ["App/Tests/UITests/**"],
             dependencies: [.target(name: appName)]
         )
-    ],
-    packages: [
-        .remote(url: "https://github.com/groue/GRDB.swift", requirement: .upToNextMajor(from: "6.0.0")),
-        .remote(url: "https://github.com/kean/Pulse", requirement: .upToNextMajor(from: "4.0.0"))
     ]
 )
